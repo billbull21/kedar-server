0db820ca3951f3ff6fd31ddf2e71ab933ee5d7f3
khabib <oemampedia@gmail.com>
2021-10-31 02:52:49 +0700

    add middleware to fetchUserByToken

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const authController = require('../controllers/auth/auth_controller');
 const verifyToken = require('../middlewares/verifyToken');
 
-router.get('/:username', authController.fetchUserByUsername);
+router.get('/:username', verifyToken, authController.fetchUserByUsername);
 router.put('/:username', verifyToken, authController.updateUser);
-router.get('/all', authController.fetchAllUsers);
-router.get('/', authController.fetchUserByToken);
+router.get('/all', verifyToken, authController.fetchAllUsers);
+router.get('/', verifyToken, authController.fetchUserByToken);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/confirm/:confirmationCode', authController.confirm);
